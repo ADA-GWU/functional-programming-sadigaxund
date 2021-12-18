937e3fdc5be892e1e22b73d55b701a4d8e7361ff
Sadig Akhund <sadigaxund@gmail.com>
2021-12-19 00:55:04 +0400

    Added 2nd method for Ruby

--- Option A/Ruby/main.rb
+++ Option A/Ruby/main.rb
@@ -1,3 +1,21 @@
-arr = [1, 2, 3, 4, 5, 6]
-arr = arr.each_index.map {|i| arr[-1 - i]} # seems pretty functional, no?
-puts "[".concat(arr.join(', ')).concat("]")
+# Method 1
+def reverse1(arr)
+    retval = []
+    arr.each do |e| 
+        # retval.unshift(*[e])
+        retval =  [e] + retval[0..retval.length]
+    end 
+    return retval
+end
+
+# Method 2
+def reverse2(arr)
+    arr.each_index.map {|i| arr[-1 - i]}
+end
+
+arr = [1, 2, 3, 4, 5]
+res = reverse1(arr)  
+# res = reverse2(arr) 
+
+puts "[" + res * ", " + "]"
+ 
