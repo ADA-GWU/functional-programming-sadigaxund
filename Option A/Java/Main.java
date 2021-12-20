a0c415699ea391630896456fc71e9852aa5582ab
Sadig Akhund <sadigaxund@gmail.com>
2021-12-20 23:19:58 +0400

    Modified Java Code

--- Option A/Java/Main.java
+++ Option A/Java/Main.java
@@ -1,18 +1,24 @@
-public class Main
-{
-	public static void main(String[] args) {
-	    int[] A = new int[]{1, 2, 3, 4, 5}; // initialize a list(array)
-	    int n = 5; // size
-	    
-        /* Reverse the array */
-	    for(int i = 0; i < n / 2; i++){
-            int temp = A[i];
-            A[i] = A[n - i - 1];
-            A[n - i - 1] = temp;
-        }   
-	    /* Print the array */
-	    for(int e : A)
-	        System.out.print(e + " ");
-	    
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// scan
+	Scanner sc = new Scanner(System.in);
+	String[] A = sc.nextLine().split(" ");
+	sc.close();
+
+	int n = A.length; // size of array
+	/* Reverse */
+	for (int i = 0; i < n / 2; i++) {
+		// swap
+	    String temp = A[i];
+	    A[i] = A[n - i - 1];
+	    A[n - i - 1] = temp;
 	}
+	// print
+	for (String e : A)
+	    System.out.print(e + " ");
+
+    }
 }
